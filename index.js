a2c3ef36d9538675e92bbc8a2dd8d6398b44ad32
Vương Quốc Hưng <qhung8a@gmail.com>
2026-01-31 23:47:33 +0700

    tích hợp pug

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+// Thiết lập thư mujc views
+app.set('views', path.join(__dirname, 'views'));
+
+// Thiết lập view engine pug 
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.send('Trang chủ hello')
+  res.render('client/pages/home', {
+    pageTitle: 'Trang chủ 123',
+  });
 })
 
 app.get('/tours', (req, res) => {
-  res.send('Danh sách tour')
+  res.render('client/pages/tour-list', {
+    pageTitle: 'Dang sách tour du lịch',
+  });
 })
 
 app.listen(port, () => {
