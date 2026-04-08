8ef6c92e8a311cb2f5aa82e75e4a7329d0643d3f
Vương Quốc Hưng <qhung8a@gmail.com>
2026-04-08 17:42:31 +0700

    Hiển thị data ra top header bên user

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const tourRoutes = require("./tour.route");
 const homeRoutes = require("./home.route");
-const cartRoutes = require("./cart.route")
+const cartRoutes = require("./cart.route");
+const settingMiddleware = require("../../middlewares/admin/setting.middleware");
 
+router.use(settingMiddleware.websiteInfo)
 router.use('/', homeRoutes);
 router.use('/tours', tourRoutes);
 router.use('/cart', cartRoutes);
