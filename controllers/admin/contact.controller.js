cb756cca3362b573dde234a6fcddfebb11d4cf88
Vương Quốc Hưng <qhung8a@gmail.com>
2026-04-12 16:49:40 +0700

    Tính năng change multi và delete cho contact list

--- controllers/admin/contact.controller.js
+++ controllers/admin/contact.controller.js
@@ -19,3 +19,64 @@ module.exports.list = async (req, res) => {
     contactList: contactList
   });
 }
+
+module.exports.deletePatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const contactDetail = await Contact.findOne({
+      _id: id,
+      deleted: false
+    });
+
+    if(!contactDetail) {
+      res.json({
+        code: "error",
+        message: "Thông tin liên hệ không tồn tại!"
+      })
+      return;
+    }
+    await Contact.updateOne({
+      _id: id
+    }, {
+      deleted: true,
+      deletedBy: req.account.id,
+      deletedAt: Date.now()
+    })
+
+    res.json({
+      code: "success",
+      message: "Xóa thông tin liên hệ thành công!"
+    });
+
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!"
+    });
+  }
+}
+
+module.exports.changeMultiPatch = async (req, res) => {
+  try {
+    const { listId, option } = req.body;
+    await Contact.updateMany({
+      _id: { $in: listId },
+      deleted: false
+    }, {
+      deleted: true,
+      deletedBy: req.account.id,
+      deletedAt: Date.now()
+    });
+    res.json({
+      code: "success",
+      message: "Đã xóa thông tin liên hệ!"
+    });
+
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!"
+    })
+  }
+}
--- routes/admin/contact.route.js
+++ routes/admin/contact.route.js
@@ -4,4 +4,8 @@ const contactController =  require('../../controllers/admin/contact.controller')
 
 router.get('/list', contactController.list);
 
+router.patch('/change-multi', contactController.changeMultiPatch);
+
+router.patch('/delete/:id', contactController.deletePatch);
+
 module.exports = router;  
