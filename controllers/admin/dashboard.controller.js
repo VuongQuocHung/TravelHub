c250f99b1c4d170cb31d7cdbeedc36b07da38480
Vương Quốc Hưng <qhung8a@gmail.com>
2026-05-28 23:23:19 +0700

    Làm trang tổng quan

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,30 @@
+const AccountAdmin = require('../../models/account-admin.model');
+const Order = require('../../models/order.model');
+module.exports.dashboard = async (req, res) => {
+  const overview = {
+    user: 0,
+    admin: 0,
+    order: 0,
+    revenue: 0
+  }
 
-module.exports.dashboard = (req, res) => {
+  overview.admin = await AccountAdmin.countDocuments({
+    deleted: false
+  });
+
+  overview.order = await Order.countDocuments({
+    deleted: false
+  })
+
+  const orderList = await Order.find({
+    deleted: false,
+    paymentStatus: "paid",
+    status: "done"
+  })
+  overview.revenue = orderList.reduce((total, order) => total + order.total, 0);
+  
   res.render('admin/pages/dashboard', {
     pageTitle: 'Trang dashboard',
+    overview: overview
   });
 }
--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -12,19 +12,21 @@ module.exports.list = async (req, res) => {
   console.log(orderList);
   for(const orderDetail of orderList) {
     // Phương thức thanh toán
-      orderDetail.paymentMethodName = paymentMethodList.find(item => item.value === orderDetail.paymentMethod).label;
-    
-      // Trạng thái thanh toán
-      orderDetail.paymentStatusName = paymentStatusList.find(item => item.value === orderDetail.paymentStatus).label;
-    
-      // Trạng thái đơn hàng
-      orderDetail.status = orderStatusList.find(item => item.value === orderDetail.status);
-    
-      orderDetail.createdAtFormat = moment(orderDetail.createdAt).format("HH:mm - DD/MM/YYYY");
+    orderDetail.paymentMethodName = paymentMethodList.find(item => item.value === orderDetail.paymentMethod).label;
+  
+    // Trạng thái thanh toán
+    orderDetail.paymentStatusName = paymentStatusList.find(item => item.value === orderDetail.paymentStatus).label;
+  
+    // Trạng thái đơn hàng
+    orderDetail.statusDetail = orderStatusList.find(item => item.value === orderDetail.status);
+
+    // Ngày đặt
+    orderDetail.createdAtFormat = moment(orderDetail.createdAt).format("HH:mm - DD/MM/YYYY");
   }
   res.render('admin/pages/order-list', {
     pageTitle: 'Trang danh sách đơn hàng',
-    orderList: orderList
+    orderList: orderList,
+
   });
 }
 
@@ -47,7 +49,7 @@ module.exports.edit = async (req, res) => {
       });
       tour.locationFrom = city.name;
     }
-    console.log("detail", orderDetail.createdAtFormat);
+
     res.render('admin/pages/order-edit', {
       pageTitle: `Đơn hàng: ${orderDetail.code}`,
       orderDetail: orderDetail,
