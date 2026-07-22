8f7760e6d6e7caa9146247353ab2aa5006294978
Vương Quốc Hưng <qhung8a@gmail.com>
2026-07-22 15:20:12 +0700

    update

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -4,12 +4,78 @@ const City = require("../../models/city.model");
 const moment = require('moment');
 
 module.exports.list = async (req, res) => {
-  const orderList = await Order.find({
+  const target = {
     deleted: false
-  }).sort({
-    createdAt: "desc"
-  })
-  console.log(orderList);
+  };
+
+  // Lọc theo trạng thái
+  if(req.query.status){
+    target.status = req.query.status
+  }
+  // Hết lọc theo trạng thái
+
+  // Lọc theo ngày 
+  if(req.query.fromDate){
+    target.createdAt = {
+      $gte: new Date(req.query.fromDate)
+    }
+  }
+
+  if(req.query.toDate){
+    target.createdAt = {
+      ...target.createdAt,
+      $lte: new Date(req.query.toDate)
+    }
+  }
+  // Hết lọc theo ngày
+
+  // Lọc theo phương thức thanh toán
+  if(req.query.paymentMethod){
+    target.paymentMethod = req.query.paymentMethod
+  }
+  // Hết lọc theo phương thức thanh toán
+
+  // Lọc theo trạng thái thanh toán
+  if(req.query.paymentStatus){
+    target.paymentStatus = req.query.paymentStatus
+  }
+  // Hết lọc theo trạng thái thanh toán
+
+  // Tìm kiếm
+  if(req.query.keyword){
+    const keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    target.code = regex;
+  }
+  // Hết Tìm Kiếm
+
+  // Pagination
+  const limit = 5;
+  let page = 1;
+  if(req.query.page) {
+    const currentPage = parseInt(req.query.page);
+    if(currentPage > 0) {
+      page = currentPage;
+    }
+  }
+  const skip = (page - 1) * limit;
+  const totalRecord = await Order.countDocuments(target);
+  const totalPage = Math.ceil(totalRecord/limit);
+  const paginationData = {
+    skip: skip,
+    totalRecord: totalRecord,
+    totalPage: totalPage
+  };
+  // Hết Pagination
+
+  const orderList = await Order
+    .find(target)
+    .sort({
+      createdAt: "desc"
+    })
+    .limit(limit)
+    .skip(skip);
+
   for(const orderDetail of orderList) {
     // Phương thức thanh toán
     orderDetail.paymentMethodName = paymentMethodList.find(item => item.value === orderDetail.paymentMethod).label;
@@ -23,10 +89,13 @@ module.exports.list = async (req, res) => {
     // Ngày đặt
     orderDetail.createdAtFormat = moment(orderDetail.createdAt).format("HH:mm - DD/MM/YYYY");
   }
+
   res.render('admin/pages/order-list', {
     pageTitle: 'Trang danh sách đơn hàng',
     orderList: orderList,
-
+    paymentMethodList: paymentMethodList,
+    paymentStatusList: paymentStatusList,
+    paginationData: paginationData
   });
 }
 
